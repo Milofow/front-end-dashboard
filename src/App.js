d789a2d037728a649718ca167d4379dd48fe115f
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2021-10-13 09:12:24 +0200

    added text changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import "./App.css";
 import LoginButton from "./components/LoginButton";
-<<<<<<< HEAD
 import Dashboard from "./components/Dashboard";
-=======
-import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
->>>>>>> a22281bad55fd1d3b41b2477623ecbb0aa504f54
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { isAuthenticated } = useAuth0();
-<<<<<<< HEAD
   return <>{isAuthenticated ? <Dashboard /> : <LoginButton />}</>;
-=======
-  if (isAuthenticated) {
-    return (
-      <>
-        <LogoutButton />
-        <Profile />
-      </>
-    );
-  }
-  return (
-    <>
-      <LoginButton />
-    </>
-  );
->>>>>>> a22281bad55fd1d3b41b2477623ecbb0aa504f54
 }
 
 export default App;
