5ab229f323706ddc526b137a9ae245c977aed9e2
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2022-01-20 09:33:19 +0100

    Working protection from dashboard if not logged in

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,48 @@
 import React from "react";
 import LogoutButton from "./LogoutButton";
-import Profile from "./Profile";
-import Lamp from "./Lamp";
+import axios from "axios";
+import { useState } from "react";
+import Device from "./Device";
+import { useEffect } from "react";
 
 const Dashboard = () => {
+
+  const apiUrl = "http://localhost:9096/api"
+  const [devices, setDevices] = useState(null);
+
+  const fetchDevices = () => {
+    axios
+      .get(`${apiUrl}/devices`)
+      .then(function (response) {
+        setDevices(response.data);
+      })
+      .catch(function (error) {});
+  };
+
+  useEffect(() => {
+    if(devices == null) {
+      fetchDevices();
+    } 
+  });
+
   return (
     <>
-      <div>Dashboard</div>;
+      <div>
+        <h1>Dashboard</h1>
+      </div>
       <LogoutButton />
-      <Profile />
-      <Lamp lightId="10" />
+      <div>
+      { devices != null? 
+        devices.map((device) => {
+          return (<div key={device.id} style={{display:"inline-block", margin:"0 20px"}}>
+                    <Device device={device}/>
+                  </div>
+        )})
+        :
+        <div>No devices found</div>
+        
+      }
+      </div>
     </>
   );
 };
